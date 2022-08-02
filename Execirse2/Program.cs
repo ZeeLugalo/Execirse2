20c58026e46270e8852c4100d7ac0a14ac7313cd
ZeeLugalo <4271363@myuwc.ac.za>
2022-08-02 12:18:16 +0200

    Add project files.

---
+++ Execirse2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Execirse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 4, 6, 9 };
+            bool answer = false;
+            for (int i = 0; i < myArray.Length; i++)
+            
+            {
+                if (myArray[i] % 2 == 0) 
+                {
+                    answer = false;
+                    { 
+
+                    answer=true;
+
+
+                    }
+                    Console.WriteLine("There is a odd number!");
+                }
+            }
+        }
+    }
+}
